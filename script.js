ffbc534540a21845c3f255fca8a3ed333e9451f3
Hannah <156165697+HGreen9@users.noreply.github.com>
2024-03-07 20:12:16 +0000

    template

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-// Check JS is Connected
-console.log('JS Connected');
+// JS for Plyr Plugin
+// https://cdn.plyr.io/3.7.8/plyr.js
+
+// Use it.Run it. Go. aka Instantiate
+// objects
+// data types
+// console.log('🍎');
+// console.log(123);
+// console.log(true);
+// variables
+// const name = 'Poukong';
+// let age = 50;
+// let isHappy = false;
+
+// exercise.run( 'fast' )
+// cooking.frying( 350  );
+// console.log( name );
+// console.log( age );
+// console.log( isHappy );
+
+// Make Plyr plugin go aka instantiation
+const player = new Plyr('audio', {});
